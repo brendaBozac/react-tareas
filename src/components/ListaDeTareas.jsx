5c9aa3b8e5b2daed740efe6b5c3e8ec3b957d008
brendaBozac <159967145+brendaBozac@users.noreply.github.com>
2024-07-18 16:21:05 -0300

    completar logica proyecto tareas react

--- src/components/ListaDeTareas.jsx
+++ src/components/ListaDeTareas.jsx
@@ -1,14 +1,58 @@
-import React from "react";
-import TareaFormulario from "./TareaFormulario";
+import React, { useState } from "react";
+import TareaFormulario from "./TareaFormulario.jsx";
 import '../hojas-de-estilos/ListaDeTareas.css'
+import Tarea from "./Tarea.jsx"
 
 /* las etiquetas vacias son fragmentos en react */
 function ListaDeTareas() {
+
+  const [tareas, setTareas] = useState([]);
+
+  const agregarTarea = tarea => {
+    /* estamos comprobando que la cadena no esta vacia primero
+    trim es un metodo para qitar espacios vacios al comienzo y final de la string */
+    if (tarea.texto.trim()) {
+      tarea.texto = tarea.texto.trim();
+      /* aqui vamos a mostrar la tarea nueva y las anteriores, con ...spred desglosamos las tareas antiguas individuales
+      como objetos del array */
+      const tareasActualizadas = [tarea, ...tareas];
+      setTareas(tareasActualizadas);
+
+    }
+  }
+
+  /* map necesita que retornemos un valor */
+  const completarTarea = id => {
+    const tareasActualizadas = tareas.map(tarea => {
+      if (tarea.id === id) {
+        tarea.completada = !tarea.completada;
+      }
+      return tarea;
+    });
+    setTareas(tareasActualizadas);
+  }
+
+  const eliminarTarea = id => {
+    const tareasActualizadas = tareas.filter(tarea => tarea.id !== id);
+    setTareas(tareasActualizadas);
+  }
+
   return (
     <>
-      <TareaFormulario />
+      <TareaFormulario onSubmit={agregarTarea}/>
       <div className="tareas-lista-contenedor">
-          LISTA DE TAREAS
+        {
+          tareas.map((tarea) => 
+            <Tarea 
+              key={tarea.id}
+              id={tarea.id}
+              texto={tarea.texto}
+              completada={tarea.completada}
+              eliminarTarea={eliminarTarea}
+              completarTarea={completarTarea}
+            />
+          )
+        }
       </div>
 
     </>
--- src/components/TareaFormulario.jsx
+++ src/components/TareaFormulario.jsx
@@ -1,14 +1,42 @@
-import React from "react";
-import '../hojas-de-estilos/TareaFormulario.css'
+import React, { useState } from "react";
+import '../hojas-de-estilos/TareaFormulario.css';
+import {v4 as uuidv4} from "uuid";
 
 function TareaFormulario(props) {
+
+    const [input, setInput] = useState('');
+
+    const manejarCambio = e => {
+        setInput(e.target.value); /* para extraer el valor del campo de texto q ingreso el usuario */
+    }
+
+    const manejarEnvio = e => {
+        /* para no volver a cargar toooda la aplicacion, solo los cambios, usamos preventDefault */
+        e.preventDefault();
+        console.log('enviando formulario...');
+        
+        /* usamos el paquete uuid para crear identificadores unicos,
+        lo instalamos en la consolada npm install uuid, y en nuesrtro archivo lo tenemos que importar */
+        const tareaNueva = {
+            id: uuidv4(),
+            texto: input,
+            completada: false
+        }
+
+        props.onSubmit(tareaNueva);
+
+    }
     return (
-        <form className="tarea-formulario">
+        <form 
+            className="tarea-formulario"
+            /* onsubmit es un tipo de evento, como onchange, y onclick */
+            onSubmit={manejarEnvio}>
             <input 
             className="tarea-input"
             type="text"
             placeholder="Escribe una Tarea" 
             name="texto"
+            onChange={manejarCambio}
             />
             <button className="tarea-boton">
                 Agregar Tarea
